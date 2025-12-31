bce19c4ca1e9322db8c514a99548814e06192469
MarshalMelipona <solsticeofthewinter@gmail.com>
2025-12-30 17:28:08 -1000

    Update DoAfterSystem.cs

--- Content.Client/DoAfter/DoAfterSystem.cs
+++ Content.Client/DoAfter/DoAfterSystem.cs
@@ -55,8 +55,11 @@ public sealed class DoAfterSystem : SharedDoAfterSystem
             return;
 
         // this is genuinely awful and hacky, but it works, and it took me three hours and a headache
-        // before resorting to this. replace this with a better solution if you can,
-        // but prediction can genuinely ████ my ████
+        // before resorting to this. 
+        // the client wasn't getting the ignore obstructions tag even if it was set in the dargs and caused really weird
+        // flickering with the progress bar
+        // if you have a better solution, dm me at @MarshalMelipona and explain it to me so this can be rightfully taken out back and shot
+        //  prediction can genuinely ████ my ████, i'm sorry for my sins. - delphine
         var ignoreObstructions = _tag.HasTag(playerEntity.Value, _ignoreObstructionsTag); // Omu
 
         var time = GameTiming.CurTime;
