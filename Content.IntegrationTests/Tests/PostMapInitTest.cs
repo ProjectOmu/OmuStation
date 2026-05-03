7748a3b4b0e47c2fce0f37c98c5dafba3882a9dc
Willzile1 <Willzile1@gmail.com>
2026-05-03 00:10:59 -0400

    Updated Crystal Station Updated map prototype and created a map folder in _Omu Added Crystal to the MapTest.cs Made requested map changes.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -211,6 +211,7 @@ namespace Content.IntegrationTests.Tests
             "Cluster",
             "Cog",
             "Core",           // Not in pool.
+            "Crystal", // OMU
             "Delta",
             "Dev",            // Dev map
             "dm01-entryway",  // Deathmatch
