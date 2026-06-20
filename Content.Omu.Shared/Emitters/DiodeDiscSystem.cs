18cf884c1ec8bb9a578cf901d904b0cdb4548ccc
harryhulse12-alt <harryhulse12@gmail.com>
2026-06-20 19:38:30 +0100

    basics

---
+++ Content.Omu.Shared/Emitters/DiodeDiscSystem.cs
@@ -0,0 +1,9 @@
+using Content.Shared.Singularity.EntitySystems;
+using Content.Shared.DoAfter;
+using Content.Shared.Whitelist;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Omu.Shared.DiodeDiscSystem;
