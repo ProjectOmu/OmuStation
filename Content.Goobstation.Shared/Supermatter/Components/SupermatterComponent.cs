f759e043c3563ec9c9e146e4f027f7385b2658f1
harryhulse12-alt <harryhulse12@gmail.com>
2026-06-28 17:02:27 +0100

    Update SupermatterComponent.cs with comments

--- Content.Goobstation.Shared/Supermatter/Components/SupermatterComponent.cs
+++ Content.Goobstation.Shared/Supermatter/Components/SupermatterComponent.cs
@@ -29,11 +29,11 @@ using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.ViewVariables;
-using Robust.Shared.Utility;
-using Robust.Shared.Prototypes;
-using Content.Shared.Random;
-using Robust.Shared.Toolshed.TypeParsers;
-using Content.Shared.Radio;
+using Robust.Shared.Utility;        //omu
+using Robust.Shared.Prototypes;       //omu
+using Content.Shared.Random;        /omu
+using Robust.Shared.Toolshed.TypeParsers;    //omu
+using Content.Shared.Radio;        //omu
 
 namespace Content.Goobstation.Shared.Supermatter.Components;
 
