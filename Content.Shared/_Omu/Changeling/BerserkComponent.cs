1c57b48bda450618712db11ca7f234272fda5b6e
harryhulse12-alt <harryhulse12@gmail.com>
2026-07-02 11:34:54 +0100

    rageeee

---
+++ Content.Shared/_Omu/Changeling/BerserkComponent.cs
@@ -0,0 +1,22 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
+
+namespace Content.Shared._Omu.Changeling;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, AutoGenerateComponentPause]
+public sealed partial class BerserkAffectedComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public EntityUid ExcludedEntity;
+
+    [DataField]
+    public float Duration = 10f;
+
+    [DataField, AutoNetworkedField, AutoPausedField]
+    public TimeSpan NextAttack = TimeSpan.Zero;
+}
+
+public enum BerserkKey : byte
+{
+    Key,
+}
---
+++ Resources/Locale/en-US/_Omu/reagents/narcotics.ftl
@@ -0,0 +1,2 @@
+reagent-name-berserktoxin = Berserk Toxin
+reagent-desc-berserktoxin = A neurotoxin that activates the rage associated parts of the brain causing uncontrollable anger
