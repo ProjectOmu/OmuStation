ab48776ea405719bd07152a2df525d52a3683ed2
harryhulse12-alt <harryhulse12@gmail.com>
2026-07-18 12:06:54 +0100

    some cleanup and a second pr

--- Content.Server/_Mono/Claws/ClawsSystem.cs
+++ Content.Server/_Mono/Claws/ClawsSystem.cs
@@ -3,6 +3,7 @@ using Content.Shared._Mono.Claws.ClawTypes;
 using Content.Shared._Mono.Claws.Components;
 using Content.Shared.Weapons.Melee.Events;
 using Robust.Shared.Random;
+using Content.Shared.Popups;
 
 namespace Content.Server._Mono.Claws;
 
@@ -50,6 +51,9 @@ public sealed class ClawsSystem : SharedClawsSystem
 
             comp.GrowTimer = TimeSpan.Zero;
             comp.ClawStage = comp.Claws.GetValueOrDefault(TryGetStageNumber(comp) + 1);
+
+            if (comp.ClawGrowthNotification != null)
+                _popup.PopupEntity(Loc.GetString(comp.ClawGrowthNotification), uid, PopupType.Large);
 
             UpdateClaws(uid, comp);
             Dirty(uid, comp);
