new-frontiers-14/frontier-station-14

Offshore Gas Drilling and Pumping (#2403)
* gaslock wip

* gaslock WIP

* gaslock: entity's more or less good

* deposits & drills, WIP

* gas deposit drill WIP

* More work on deposits and drills, still WIP

* random gas deposits, drill fixes

* drill visualizers, ignore deposits, yaml fixes

* Gas deposits, pipe fixes for the drill

* gaslock RSI fix

* gaslock to binary.yml, joinSystem test

* GasDepositExtractor: push into net, not pipe

* Clear pump on undock

* WIP docking types, sale point

* selective docking WIP

* WIP gas sale point, console, gas sale UI

* Bidirectional pressure pipes

* gaslock console wip

* cleanup, smaller deposits

* Add gaslocks to the atmospheric alerts computer

* Fix gaslock pipe readout

* Remove unused deposits

* Undupe AtmosDirectionChanged log value

* Address test failures

* Add remaining missing containers

* Fix missing source/sink port refs in gas sale comp

* maps and maps

* fix gas drill examine text

* make storage canisters craftable

* gas price changes

* N2/O2 to zero, fix trit/frezon comment

* comment out edison for now

* Lower cargo canister prices, raise plasma

* gasbender: resolve duped IDs

* Fix invalid network config ref

* canister arbitrage fix

* Narrow gaslock, fix bidirectional pump UI overlap

* Remove GaslockReversed reference in ambition

* internal gas sale point, better piping

* max extraction rate, deposit suffixes

* gas sale point: inherit from basemachinepowered

* sale point: fix parent order

* gas sale port: dockable pump->dockable pipe

* No gas sale point alerts entry

* Rename dockablepumpsystem file, nix unneeded deps

* Dirty gas deposit extractor UI on open

* MapInit, not ComponentInit, hide unanchored docks

* Working sale point

* Gasbender: fix unpowered thrusters

* Gas sale console UI

* Appraise, not Refresh

* Edison: two online SMESes, coloured pipes

* cleanup

* f

Content.Shared/Atmos/EntitySystems/SharedGasPressurePumpSystem.cs
@@ -10,6 +10,7 @@ using Content.Shared.Power;
 using Content.Shared.Power.Components;
 using Content.Shared.Power.EntitySystems;
 using Content.Shared.UserInterface;
+using Content.Shared._NF.Atmos.Piping.Binary.Messages; // Frontier
 
 namespace Content.Shared.Atmos.EntitySystems;
 
@@ -31,6 +32,7 @@ public abstract class SharedGasPressurePumpSystem : EntitySystem
 
         SubscribeLocalEvent<GasPressurePumpComponent, GasPressurePumpChangeOutputPressureMessage>(OnOutputPressureChangeMessage);
         SubscribeLocalEvent<GasPressurePumpComponent, GasPressurePumpToggleStatusMessage>(OnToggleStatusMessage);
+        SubscribeLocalEvent<GasPressurePumpComponent, GasPressurePumpChangePumpDirectionMessage>(OnPumpSetDirectionMessage); // Frontier
 
         SubscribeLocalEvent<GasPressurePumpComponent, AtmosDeviceDisabledEvent>(OnPumpLeaveAtmosphere);
         SubscribeLocalEvent<GasPressurePumpComponent, ExaminedEvent>(OnExamined);
@@ -70,7 +72,7 @@ public abstract class SharedGasPressurePumpSystem : EntitySystem
         UpdateAppearance(uid, component);
     }
 
-    private void UpdateAppearance(EntityUid uid, GasPressurePumpComponent? pump = null, AppearanceComponent? appearance = null)
+    protected void UpdateAppearance(EntityUid uid, GasPressurePumpComponent? pump = null, AppearanceComponent? appearance = null) // Frontier: private<protected
     {
         if (!Resolve(uid, ref pump, ref appearance, false))
             return;
@@ -104,4 +106,22 @@ public abstract class SharedGasPressurePumpSystem : EntitySystem
 
         UserInterfaceSystem.CloseUi(uid, GasPressurePumpUiKey.Key);
     }
+
+    // Frontier - bidirectional pumps
+    public void OnPumpSetDirectionMessage(EntityUid uid, GasPressurePumpComponent pump, GasPressurePumpChangePumpDirectionMessage args)
+    {
+        if (!pump.SettableDirection || pump.PumpingInwards == args.Inwards)
+            return;
+
+        var temp = pump.OutletName;
+        pump.OutletName = pump.InletName;
+        pump.InletName = temp;
+
+        pump.PumpingInwards = args.Inwards;
+        _adminLogger.Add(LogType.AtmosDirectionChanged, LogImpact.Medium,
+            $"{ToPrettyString(args.Actor):player} set the direction on {ToPrettyString(uid):device} to {(args.Inwards ? "in" : "out")}");
+        Dirty(uid, pump);
+        UpdateAppearance(uid, pump);
+    }
+    // End Frontier
 }

ProjectOmu/OmuStation

Content.Shared/Atmos/EntitySystems/SharedGasPressurePumpSystem.cs
@@ -13,6 +13,8 @@ using Content.Shared.Database;
 using Content.Shared.Examine;
 using Content.Shared.Power;
 using Content.Shared.Power.EntitySystems;
+using Content.Shared.UserInterface;
+using Content.Shared._NF.Atmos.Piping.Binary.Messages; // Frontier
 
 namespace Content.Shared.Atmos.EntitySystems;
 
@@ -34,6 +36,7 @@ public abstract class SharedGasPressurePumpSystem : EntitySystem
 
         SubscribeLocalEvent<GasPressurePumpComponent, GasPressurePumpChangeOutputPressureMessage>(OnOutputPressureChangeMessage);
         SubscribeLocalEvent<GasPressurePumpComponent, GasPressurePumpToggleStatusMessage>(OnToggleStatusMessage);
+        SubscribeLocalEvent<GasPressurePumpComponent, GasPressurePumpChangePumpDirectionMessage>(OnPumpSetDirectionMessage); // Frontier
 
         SubscribeLocalEvent<GasPressurePumpComponent, AtmosDeviceDisabledEvent>(OnPumpLeaveAtmosphere);
         SubscribeLocalEvent<GasPressurePumpComponent, ExaminedEvent>(OnExamined);
@@ -61,12 +64,12 @@ public abstract class SharedGasPressurePumpSystem : EntitySystem
 
     private void OnPowerChanged(Entity<GasPressurePumpComponent> ent, ref PowerChangedEvent args)
     {
-        UpdateAppearance(ent);
+        UpdateAppearance(ent, component);
     }
 
-    private void UpdateAppearance(Entity<GasPressurePumpComponent, AppearanceComponent?> ent)
+    protected void UpdateAppearance(EntityUid ent, GasPressurePumpComponent? pump = null, AppearanceComponent? appearance = null) // Frontier: private<protected
     {
-        if (!Resolve(ent, ref ent.Comp2, false))
+        if (!Resolve(ent, ref pump, ref appearance, false))
             return;
 
         var pumpOn = ent.Comp1.Enabled && _receiver.IsPowered(ent.Owner);
@@ -106,4 +109,21 @@ public abstract class SharedGasPressurePumpSystem : EntitySystem
     protected virtual void UpdateUi(Entity<GasPressurePumpComponent> ent)
     {
     }
+    // Frontier - bidirectional pumps
+    public void OnPumpSetDirectionMessage(EntityUid uid, GasPressurePumpComponent pump, GasPressurePumpChangePumpDirectionMessage args)
+    {
+        if (!pump.SettableDirection || pump.PumpingInwards == args.Inwards)
+            return;
+
+        var temp = pump.OutletName;
+        pump.OutletName = pump.InletName;
+        pump.InletName = temp;
+
+        pump.PumpingInwards = args.Inwards;
+        _adminLogger.Add(LogType.AtmosDirectionChanged, LogImpact.Medium,
+            $"{ToPrettyString(args.Actor):player} set the direction on {ToPrettyString(uid):device} to {(args.Inwards ? "in" : "out")}");
+        Dirty(uid, pump);
+        UpdateAppearance(uid, pump);
+    }
+    // End Frontier
 }