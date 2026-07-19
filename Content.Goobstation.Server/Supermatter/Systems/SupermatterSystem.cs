fdc67a6d3d4cc0ad3ffcb9eb5762eaeba456d729
harryhulse12-alt <harryhulse12@gmail.com>
2026-07-19 20:16:36 +0100

    region fix

--- Content.Goobstation.Server/Supermatter/Systems/SupermatterSystem.cs
+++ Content.Goobstation.Server/Supermatter/Systems/SupermatterSystem.cs
@@ -844,7 +844,7 @@ public sealed class SupermatterSystem : SharedSupermatterSystem
 
     }
 
-    
+
 
     public void AdjustSetpoints(SupermatterComponent sm)
     {
@@ -881,6 +881,7 @@ public sealed class SupermatterSystem : SharedSupermatterSystem
 
             if (sm.RadiationOutputFactor == sm.RadiationOutputFactorSetpoint)
                 sm.RadiationOutputFactorChanged = false;
-        }      #endregion
-    }        //omu end
+        }
+    }
+    #endregion     //omu end
 }
