0048e98841a46cb970298edd30ab4f10eef4ec5b
harryhulse12-alt <harryhulse12@gmail.com>
2026-07-20 13:13:19 +0100

    Update GenericCounterAlertSystem.cs

--- Content.Client/Alerts/GenericCounterAlertSystem.cs
+++ Content.Client/Alerts/GenericCounterAlertSystem.cs
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2025 Coenx-flex
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 using System.Numerics;
 using Content.Shared.Alert.Components;
 using Robust.Client.GameObjects;
