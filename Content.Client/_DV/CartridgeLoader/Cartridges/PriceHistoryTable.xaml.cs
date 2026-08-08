JustTNE/Delta-v

stocks trading (#2103)
* sort of works

* fix

* oh right

* admin abuse

* buy/sell events

* price history, better error handling

* joke

* buying and selling

* access handling

* IT'S FINALLY REAL

* fixes

* AAAAAAAAAAAAAAAAA

* i keep fucking forgetting things

* FUCKING

* move company structs out of the comp

* ok im going to sleep

* Revert "oh right"

This reverts commit 8681e5a07b4d059fa7db718dd3ec6f25f0ad05d9.

* Revert "fix"

This reverts commit 3992ec99fd96dc7b2df0767e1b83d5418d86a248.

* address reviews

Content.Client/DeltaV/CartridgeLoader/Cartridges/PriceHistoryTable.xaml.cs
@@ -1,0 +1,75 @@
+using System.Linq;
+using Robust.Client.AutoGenerated;
+using Robust.Client.Graphics;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+
+namespace Content.Client.DeltaV.CartridgeLoader.Cartridges;
+
+[GenerateTypedNameReferences]
+public sealed partial class PriceHistoryTable : BoxContainer
+{
+    public PriceHistoryTable()
+    {
+        RobustXamlLoader.Load(this);
+
+        // Create the stylebox here so we can use the colors from StockTradingUi
+        var styleBox = new StyleBoxFlat
+        {
+            BackgroundColor = StockTradingUiFragment.PriceBackgroundColor,
+            ContentMarginLeftOverride = 6,
+            ContentMarginRightOverride = 6,
+            ContentMarginTopOverride = 4,
+            ContentMarginBottomOverride = 4,
+            BorderColor = StockTradingUiFragment.BorderColor,
+            BorderThickness = new Thickness(1),
+        };
+
+        HistoryPanel.PanelOverride = styleBox;
+    }
+
+    public void Update(List<float> priceHistory)
+    {
+        PriceGrid.RemoveAllChildren();
+
+        // Take last 5 prices
+        var lastFivePrices = priceHistory.TakeLast(5).ToList();
+
+        for (var i = 0; i < lastFivePrices.Count; i++)
+        {
+            var price = lastFivePrices[i];
+            var previousPrice = i > 0 ? lastFivePrices[i - 1] : price;
+            var priceChange = ((price - previousPrice) / previousPrice) * 100;
+
+            var entryContainer = new BoxContainer
+            {
+                Orientation = LayoutOrientation.Vertical,
+                MinWidth = 80,
+                HorizontalAlignment = HAlignment.Center,
+            };
+
+            var priceLabel = new Label
+            {
+                Text = $"${price:F2}",
+                HorizontalAlignment = HAlignment.Center,
+            };
+
+            var changeLabel = new Label
+            {
+                Text = $"{(priceChange >= 0 ? "+" : "")}{priceChange:F2}%",
+                HorizontalAlignment = HAlignment.Center,
+                StyleClasses = { "LabelSubText" },
+                Modulate = priceChange switch
+                {
+                    > 0 => StockTradingUiFragment.PositiveColor,
+                    < 0 => StockTradingUiFragment.NegativeColor,
+                    _ => StockTradingUiFragment.NeutralColor,
+                }
+            };
+
+            entryContainer.AddChild(priceLabel);
+            entryContainer.AddChild(changeLabel);
+            PriceGrid.AddChild(entryContainer);
+        }
+    }
+}

Content.Client/DeltaV/CartridgeLoader/Cartridges/StockTradingUi.cs
@@ -1,0 +1,45 @@
+using Robust.Client.UserInterface;
+using Content.Client.UserInterface.Fragments;
+using Content.Shared.CartridgeLoader;
+using Content.Shared.CartridgeLoader.Cartridges;
+
+namespace Content.Client.DeltaV.CartridgeLoader.Cartridges;
+
+public sealed partial class StockTradingUi : UIFragment
+{
+    private StockTradingUiFragment? _fragment;
+
+    public override Control GetUIFragmentRoot()
+    {
+        return _fragment!;
+    }
+
+    public override void Setup(BoundUserInterface userInterface, EntityUid? fragmentOwner)
+    {
+        _fragment = new StockTradingUiFragment();
+
+        _fragment.OnBuyButtonPressed += (company, amount) =>
+        {
+            SendStockTradingUiMessage(StockTradingUiAction.Buy, company, amount, userInterface);
+        };
+        _fragment.OnSellButtonPressed += (company, amount) =>
+        {
+            SendStockTradingUiMessage(StockTradingUiAction.Sell, company, amount, userInterface);
+        };
+    }
+
+    public override void UpdateState(BoundUserInterfaceState state)
+    {
+        if (state is StockTradingUiState cast)
+        {
+            _fragment?.UpdateState(cast);
+        }
+    }
+
+    private static void SendStockTradingUiMessage(StockTradingUiAction action, int company, float amount, BoundUserInterface userInterface)
+    {
+        var newsMessage = new StockTradingUiMessageEvent(action, company, amount);
+        var message = new CartridgeUiMessage(newsMessage);
+        userInterface.SendMessage(message);
+    }
+}

Content.Server/DeltaV/Cargo/Components/StationStockMarketComponent.cs
@@ -1,0 +1,71 @@
+using System.Numerics;
+using Content.Server.DeltaV.Cargo.Systems;
+using Content.Server.DeltaV.CartridgeLoader.Cartridges;
+using Content.Shared.CartridgeLoader.Cartridges;
+using Robust.Shared.Audio;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Timing;
+
+namespace Content.Server.DeltaV.Cargo.Components;
+
+[RegisterComponent, AutoGenerateComponentPause]
+[Access(typeof(StockMarketSystem), typeof(StockTradingCartridgeSystem))]
+public sealed partial class StationStockMarketComponent : Component
+{
+    /// <summary>
+    /// The list of companies you can invest in
+    /// </summary>
+    [DataField]
+    public List<StockCompanyStruct> Companies = [];
+
+    /// <summary>
+    /// The list of shares owned by the station
+    /// </summary>
+    [DataField]
+    public Dictionary<int, int> StockOwnership = new();
+
+    /// <summary>
+    /// The interval at which the stock market updates
+    /// </summary>
+    [DataField]
+    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(600); // 10 minutes
+
+    /// <summary>
+    /// The <see cref="IGameTiming.CurTime"/> timespan of next update.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    [AutoPausedField]
+    public TimeSpan NextUpdate = TimeSpan.Zero;
+
+    /// <summary>
+    /// The sound to play after selling or buying stocks
+    /// </summary>
+    [DataField]
+    public SoundSpecifier ConfirmSound = new SoundPathSpecifier("/Audio/Effects/Cargo/ping.ogg");
+
+    /// <summary>
+    /// The sound to play if the don't have access to buy or sell stocks
+    /// </summary>
+    [DataField]
+    public SoundSpecifier DenySound = new SoundPathSpecifier("/Audio/Effects/Cargo/buzz_sigh.ogg");
+
+    // These work well as presets but can be changed in the yaml
+    [DataField]
+    public List<MarketChange> MarketChanges =
+    [
+        new() { Chance = 0.86f, Range = new Vector2(-0.05f, 0.05f) }, // Minor
+        new() { Chance = 0.10f, Range = new Vector2(-0.3f, 0.2f) }, // Moderate
+        new() { Chance = 0.03f, Range = new Vector2(-0.5f, 1.5f) }, // Major
+        new() { Chance = 0.01f, Range = new Vector2(-0.9f, 4.0f) }, // Catastrophic
+    ];
+}
+
+[DataDefinition]
+public sealed partial class MarketChange
+{
+    [DataField(required: true)]
+    public float Chance;
+
+    [DataField(required: true)]
+    public Vector2 Range;
+}

Content.Server/DeltaV/CartridgeLoader/Cartridges/StockTradingCartridgeSystem.cs
@@ -1,0 +1,101 @@
+using System.Linq;
+using Content.Server.Cargo.Components;
+using Content.Server.DeltaV.Cargo.Components;
+using Content.Server.DeltaV.Cargo.Systems;
+using Content.Server.Station.Systems;
+using Content.Server.CartridgeLoader;
+using Content.Shared.Cargo.Components;
+using Content.Shared.CartridgeLoader;
+using Content.Shared.CartridgeLoader.Cartridges;
+
+namespace Content.Server.DeltaV.CartridgeLoader.Cartridges;
+
+public sealed class StockTradingCartridgeSystem : EntitySystem
+{
+    [Dependency] private readonly CartridgeLoaderSystem _cartridgeLoader = default!;
+    [Dependency] private readonly StationSystem _station = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<StockTradingCartridgeComponent, CartridgeUiReadyEvent>(OnUiReady);
+        SubscribeLocalEvent<StockMarketUpdatedEvent>(OnStockMarketUpdated);
+        SubscribeLocalEvent<StationStockMarketComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<StockTradingCartridgeComponent, BankBalanceUpdatedEvent>(OnBalanceUpdated);
+    }
+
+    private void OnBalanceUpdated(Entity<StockTradingCartridgeComponent> ent, ref BankBalanceUpdatedEvent args)
+    {
+            UpdateAllCartridges(args.Station);
+    }
+
+    private void OnUiReady(Entity<StockTradingCartridgeComponent> ent, ref CartridgeUiReadyEvent args)
+    {
+        UpdateUI(ent, args.Loader);
+    }
+
+    private void OnStockMarketUpdated(StockMarketUpdatedEvent args)
+    {
+        UpdateAllCartridges(args.Station);
+    }
+
+    private void OnMapInit(Entity<StationStockMarketComponent> ent, ref MapInitEvent args)
+    {
+        // Initialize price history for each company
+        for (var i = 0; i < ent.Comp.Companies.Count; i++)
+        {
+            var company = ent.Comp.Companies[i];
+
+            // Create initial price history using base price
+            company.PriceHistory = new List<float>();
+            for (var j = 0; j < 5; j++)
+            {
+                company.PriceHistory.Add(company.BasePrice);
+            }
+
+            ent.Comp.Companies[i] = company;
+        }
+
+        if (_station.GetOwningStation(ent.Owner) is { } station)
+            UpdateAllCartridges(station);
+    }
+
+    private void UpdateAllCartridges(EntityUid station)
+    {
+        var query = EntityQueryEnumerator<StockTradingCartridgeComponent, CartridgeComponent>();
+        while (query.MoveNext(out var uid, out var comp, out var cartridge))
+        {
+            if (cartridge.LoaderUid is not { } loader || comp.Station != station)
+                continue;
+            UpdateUI((uid, comp), loader);
+        }
+    }
+
+    private void UpdateUI(Entity<StockTradingCartridgeComponent> ent, EntityUid loader)
+    {
+        if (_station.GetOwningStation(loader) is { } station)
+            ent.Comp.Station = station;
+
+        if (!TryComp<StationStockMarketComponent>(ent.Comp.Station, out var stockMarket) ||
+            !TryComp<StationBankAccountComponent>(ent.Comp.Station, out var bankAccount))
+            return;
+
+        // Convert company data to UI state format
+        var entries = stockMarket.Companies.Select(company => new StockCompanyStruct(
+            displayName: company.LocalizedDisplayName,
+                currentPrice: company.CurrentPrice,
+                basePrice: company.BasePrice,
+                priceHistory: company.PriceHistory))
+            .ToList();
+
+        // Send the UI state with balance and owned stocks
+        var state = new StockTradingUiState(
+            entries: entries,
+            ownedStocks: stockMarket.StockOwnership,
+            balance: bankAccount.Balance
+        );
+
+        _cartridgeLoader.UpdateCartridgeUiState(loader, state);
+    }
+}

ProjectOmu/OmuStation

Content.Client/_DV/CartridgeLoader/Cartridges/PriceHistoryTable.xaml.cs
@@ -1,0 +1,75 @@
+using System.Linq;
+using Robust.Client.AutoGenerated;
+using Robust.Client.Graphics;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+
+namespace Content.Client._DV.CartridgeLoader.Cartridges;
+
+[GenerateTypedNameReferences]
+public sealed partial class PriceHistoryTable : BoxContainer
+{
+    public PriceHistoryTable()
+    {
+        RobustXamlLoader.Load(this);
+
+        // Create the stylebox here so we can use the colors from StockTradingUi
+        var styleBox = new StyleBoxFlat
+        {
+            BackgroundColor = StockTradingUiFragment.PriceBackgroundColor,
+            ContentMarginLeftOverride = 6,
+            ContentMarginRightOverride = 6,
+            ContentMarginTopOverride = 4,
+            ContentMarginBottomOverride = 4,
+            BorderColor = StockTradingUiFragment.BorderColor,
+            BorderThickness = new Thickness(1),
+        };
+
+        HistoryPanel.PanelOverride = styleBox;
+    }
+
+    public void Update(List<float> priceHistory)
+    {
+        PriceGrid.RemoveAllChildren();
+
+        // Take last 5 prices
+        var lastFivePrices = priceHistory.TakeLast(5).ToList();
+
+        for (var i = 0; i < lastFivePrices.Count; i++)
+        {
+            var price = lastFivePrices[i];
+            var previousPrice = i > 0 ? lastFivePrices[i - 1] : price;
+            var priceChange = ((price - previousPrice) / previousPrice) * 100;
+
+            var entryContainer = new BoxContainer
+            {
+                Orientation = LayoutOrientation.Vertical,
+                MinWidth = 80,
+                HorizontalAlignment = HAlignment.Center,
+            };
+
+            var priceLabel = new Label
+            {
+                Text = $"${price:F2}",
+                HorizontalAlignment = HAlignment.Center,
+            };
+
+            var changeLabel = new Label
+            {
+                Text = $"{(priceChange >= 0 ? "+" : "")}{priceChange:F2}%",
+                HorizontalAlignment = HAlignment.Center,
+                StyleClasses = { "LabelSubText" },
+                Modulate = priceChange switch
+                {
+                    > 0 => StockTradingUiFragment.PositiveColor,
+                    < 0 => StockTradingUiFragment.NegativeColor,
+                    _ => StockTradingUiFragment.NeutralColor,
+                }
+            };
+
+            entryContainer.AddChild(priceLabel);
+            entryContainer.AddChild(changeLabel);
+            PriceGrid.AddChild(entryContainer);
+        }
+    }
+}

Content.Client/_DV/CartridgeLoader/Cartridges/StockTradingUi.cs
@@ -1,0 +1,45 @@
+using Robust.Client.UserInterface;
+using Content.Client.UserInterface.Fragments;
+using Content.Shared.CartridgeLoader;
+using Content.Shared.CartridgeLoader.Cartridges;
+
+namespace Content.Client._DV.CartridgeLoader.Cartridges;
+
+public sealed partial class StockTradingUi : UIFragment
+{
+    private StockTradingUiFragment? _fragment;
+
+    public override Control GetUIFragmentRoot()
+    {
+        return _fragment!;
+    }
+
+    public override void Setup(BoundUserInterface userInterface, EntityUid? fragmentOwner)
+    {
+        _fragment = new StockTradingUiFragment();
+
+        _fragment.OnBuyButtonPressed += (company, amount) =>
+        {
+            SendStockTradingUiMessage(StockTradingUiAction.Buy, company, amount, userInterface);
+        };
+        _fragment.OnSellButtonPressed += (company, amount) =>
+        {
+            SendStockTradingUiMessage(StockTradingUiAction.Sell, company, amount, userInterface);
+        };
+    }
+
+    public override void UpdateState(BoundUserInterfaceState state)
+    {
+        if (state is StockTradingUiState cast)
+        {
+            _fragment?.UpdateState(cast);
+        }
+    }
+
+    private static void SendStockTradingUiMessage(StockTradingUiAction action, int company, float amount, BoundUserInterface userInterface)
+    {
+        var newsMessage = new StockTradingUiMessageEvent(action, company, amount);
+        var message = new CartridgeUiMessage(newsMessage);
+        userInterface.SendMessage(message);
+    }
+}

Content.Server/_DV/Cargo/Components/StationStockMarketComponent.cs
@@ -1,0 +1,71 @@
+using System.Numerics;
+using Content.Server._DV.Cargo.Systems;
+using Content.Server._DV.CartridgeLoader.Cartridges;
+using Content.Shared.CartridgeLoader.Cartridges;
+using Robust.Shared.Audio;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Timing;
+
+namespace Content.Server._DV.Cargo.Components;
+
+[RegisterComponent, AutoGenerateComponentPause]
+[Access(typeof(StockMarketSystem), typeof(StockTradingCartridgeSystem))]
+public sealed partial class StationStockMarketComponent : Component
+{
+    /// <summary>
+    /// The list of companies you can invest in
+    /// </summary>
+    [DataField]
+    public List<StockCompanyStruct> Companies = [];
+
+    /// <summary>
+    /// The list of shares owned by the station
+    /// </summary>
+    [DataField]
+    public Dictionary<int, int> StockOwnership = new();
+
+    /// <summary>
+    /// The interval at which the stock market updates
+    /// </summary>
+    [DataField]
+    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(600); // 10 minutes
+
+    /// <summary>
+    /// The <see cref="IGameTiming.CurTime"/> timespan of next update.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    [AutoPausedField]
+    public TimeSpan NextUpdate = TimeSpan.Zero;
+
+    /// <summary>
+    /// The sound to play after selling or buying stocks
+    /// </summary>
+    [DataField]
+    public SoundSpecifier ConfirmSound = new SoundPathSpecifier("/Audio/Effects/Cargo/ping.ogg");
+
+    /// <summary>
+    /// The sound to play if the don't have access to buy or sell stocks
+    /// </summary>
+    [DataField]
+    public SoundSpecifier DenySound = new SoundPathSpecifier("/Audio/Effects/Cargo/buzz_sigh.ogg");
+
+    // These work well as presets but can be changed in the yaml
+    [DataField]
+    public List<MarketChange> MarketChanges =
+    [
+        new() { Chance = 0.86f, Range = new Vector2(-0.05f, 0.05f) }, // Minor
+        new() { Chance = 0.10f, Range = new Vector2(-0.3f, 0.2f) }, // Moderate
+        new() { Chance = 0.03f, Range = new Vector2(-0.5f, 1.5f) }, // Major
+        new() { Chance = 0.01f, Range = new Vector2(-0.9f, 4.0f) }, // Catastrophic
+    ];
+}
+
+[DataDefinition]
+public sealed partial class MarketChange
+{
+    [DataField(required: true)]
+    public float Chance;
+
+    [DataField(required: true)]
+    public Vector2 Range;
+}

Content.Server/_DV/CartridgeLoader/Cartridges/StockTradingCartridgeSystem.cs
@@ -1,0 +1,104 @@
+using System.Linq;
+using Content.Server.Cargo.Components;
+using Content.Server._DV.Cargo.Components;
+using Content.Server._DV.Cargo.Systems;
+using Content.Server.Station.Systems;
+using Content.Server.CartridgeLoader;
+using Content.Shared.Cargo.Components;
+using Content.Shared.CartridgeLoader;
+using Content.Shared.CartridgeLoader.Cartridges;
+using Content.Shared.Cargo;
+
+namespace Content.Server._DV.CartridgeLoader.Cartridges;
+
+public sealed class StockTradingCartridgeSystem : EntitySystem
+{
+    [Dependency] private readonly CartridgeLoaderSystem _cartridgeLoader = default!;
+    [Dependency] private readonly StationSystem _station = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<StockTradingCartridgeComponent, CartridgeUiReadyEvent>(OnUiReady);
+        SubscribeLocalEvent<StockMarketUpdatedEvent>(OnStockMarketUpdated);
+        SubscribeLocalEvent<StationStockMarketComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<StockTradingCartridgeComponent, BankBalanceUpdatedEvent>(OnBalanceUpdated);
+    }
+
+    private void OnBalanceUpdated(Entity<StockTradingCartridgeComponent> ent, ref BankBalanceUpdatedEvent args)
+    {
+        UpdateAllCartridges(args.Station);
+    }
+
+    private void OnUiReady(Entity<StockTradingCartridgeComponent> ent, ref CartridgeUiReadyEvent args)
+    {
+        UpdateUI(ent, args.Loader);
+    }
+
+    private void OnStockMarketUpdated(StockMarketUpdatedEvent args)
+    {
+        UpdateAllCartridges(args.Station);
+    }
+
+    private void OnMapInit(Entity<StationStockMarketComponent> ent, ref MapInitEvent args)
+    {
+        // Initialize price history for each company
+        for (var i = 0; i < ent.Comp.Companies.Count; i++)
+        {
+            var company = ent.Comp.Companies[i];
+
+            // Create initial price history using base price
+            company.PriceHistory = new List<float>();
+            for (var j = 0; j < 5; j++)
+            {
+                company.PriceHistory.Add(company.BasePrice);
+            }
+
+            ent.Comp.Companies[i] = company;
+        }
+
+        if (_station.GetOwningStation(ent.Owner) is { } station)
+            UpdateAllCartridges(station);
+    }
+
+    private void UpdateAllCartridges(EntityUid station)
+    {
+        var query = EntityQueryEnumerator<StockTradingCartridgeComponent, CartridgeComponent>();
+        while (query.MoveNext(out var uid, out var comp, out var cartridge))
+        {
+            if (cartridge.LoaderUid is not { } loader || comp.Station != station)
+                continue;
+            UpdateUI((uid, comp), loader);
+        }
+    }
+
+    private void UpdateUI(Entity<StockTradingCartridgeComponent> ent, EntityUid loader)
+    {
+        if (_station.GetOwningStation(loader) is { } station)
+            ent.Comp.Station = station;
+
+        if (!TryComp<StationStockMarketComponent>(ent.Comp.Station, out var stockMarket) ||
+            !TryComp<StationBankAccountComponent>(ent.Comp.Station, out var stationBank))
+            return;
+
+        var accountBalance = stationBank.Accounts.GetValueOrDefault(ent.Comp.Account);      // Omu fix since we use seperate bank accounts for each dept
+
+        // Convert company data to UI state format
+        var entries = stockMarket.Companies.Select(company => new StockCompanyStruct(
+            displayName: company.LocalizedDisplayName,
+                currentPrice: company.CurrentPrice,
+                basePrice: company.BasePrice,
+                priceHistory: company.PriceHistory))
+            .ToList();
+
+        // Send the UI state with balance and owned stocks
+        var state = new StockTradingUiState(
+            entries: entries,
+            ownedStocks: stockMarket.StockOwnership,
+            balance: accountBalance
+        );
+
+        _cartridgeLoader.UpdateCartridgeUiState(loader, state);
+    }
+}