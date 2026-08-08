48b25d650ea1f9aa22f6d67345b52d7cd4187c18
harryhulse12-alt <harryhulse12@gmail.com>
2026-08-08 21:44:59 +0100

    bit more

--- Content.Omu.Server/Entities/Heretic/LodestoneComponent.cs
+++ Content.Omu.Server/Entities/Heretic/LodestoneComponent.cs
@@ -1,6 +1,10 @@
+using Robust.Shared.Prototypes;
+
 namespace Content.Omu.Server.Entities.Heretic;
 
 [RegisterComponent, Access(typeof(LodestoneSystem))]
 public sealed partial class LodestoneComponent : Component
 {
+    [DataField]
+    public ComponentRegistry? AddedComponents;
 }
