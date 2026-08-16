f4d40aff2d216f3db7db268afcaffbe96a4c6d3b
harryhulse12-alt <harryhulse12@gmail.com>
2026-08-16 18:58:59 +0100

    Valid change

--- Content.Shared/_Lavaland/Movement/SharedHierophantBeatSystem.cs
+++ Content.Shared/_Lavaland/Movement/SharedHierophantBeatSystem.cs
@@ -20,13 +20,13 @@ public sealed class HierophantBeatSystem : EntitySystem
 
     private void OnStartup(EntityUid uid, HierophantBeatComponent component, ref ComponentStartup args)
     {
-        EnsureComp<OldTileMovementComponent>(uid);
+        EnsureComp<TileMovementComponent>(uid);
         _alertsSystem.ShowAlert(uid, component.HierophantBeatAlertId);
     }
 
     private void OnRemove(EntityUid uid, HierophantBeatComponent component, ref ComponentRemove args)
     {
-        RemComp<OldTileMovementComponent>(uid);
+        RemComp<TileMovementComponent>(uid);
         _alertsSystem.ClearAlert(uid, component.HierophantBeatAlertId);
     }
 
