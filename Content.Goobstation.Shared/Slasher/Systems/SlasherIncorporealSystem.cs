968e9c3d10dfb2362023024268437dd3d9bb7909
harryhulse12-alt <harryhulse12@gmail.com>
2026-08-17 00:36:39 +0100

    Calling it a night. Commented goobstation shared phased system startup - duplicate subscription. Replaces goobstation phase shift with generic one so it works. Comments serverside damageotheronhit infavor of shared. Adds whetsone comp/system. Adds spawn effects to shared phaseshiftcomp. Adds remove embed event - changes embedpassivedamage to use this and raises it in shared projectile system. comments a comp from bloodcultaura

--- Content.Goobstation.Shared/Slasher/Systems/SlasherIncorporealSystem.cs
+++ Content.Goobstation.Shared/Slasher/Systems/SlasherIncorporealSystem.cs
@@ -1,4 +1,4 @@
-using Content.Goobstation.Shared.PhaseShift;
+using Content.Shared.WhiteDream.BloodCult.Constructs.PhaseShift;
 using Content.Goobstation.Shared.Slasher.Components;
 using Content.Shared.Actions;
 using Content.Shared.Actions.Components;
