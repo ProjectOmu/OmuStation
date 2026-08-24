fdeb7203c3212df78a5914c029686dc88d570721
CliveOcelot <cliveocelot@gmail.com>
2026-08-24 18:48:10 +0100

    lets not target arrivals either

--- Content.Shared/_Omu/Components/CentCommBeaconComponent.cs
+++ Content.Shared/_Omu/Components/CentCommBeaconComponent.cs
@@ -2,7 +2,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._Omu.Components;
 
 /// <summary>
-/// Used to mark CC's station beacons so that they are not valid targets for things like Collosus.
+/// Used to mark CC's station beacons / warp points so that they are not valid targets for things like Collosus.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class CentCommBeaconComponent : Component;
