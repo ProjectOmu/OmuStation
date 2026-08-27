a99856961c199b1c682644fc00fcf760ee62b9d4
harryhulse12-alt <harryhulse12@gmail.com>
2026-08-27 23:40:51 +0100

    yml fixes

--- Content.Server/_Omu/Revs/SignatureConverterComponent.cs
+++ Content.Server/_Omu/Revs/SignatureConverterComponent.cs
@@ -3,5 +3,6 @@ namespace Content.Server._Omu.Revs;
 [RegisterComponent, Access(typeof(SignatureConverterSystem))]
 public sealed partial class SignatureConverterComponent : Component
 {
+    [DataField]
     public float Amount = 20f;
 }
