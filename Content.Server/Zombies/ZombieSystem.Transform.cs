d292cc970f75a9b9f7137ec3055392b8fa267262
feistyfedora <12000170+feistyfedora@users.noreply.github.com>
2026-09-01 22:07:18 -0400

    Clarify ZombieBloodProto variable comment
    Updated the comment for the ZombieBloodProto variable to clarify its purpose.

--- Content.Server/Zombies/ZombieSystem.Transform.cs
+++ Content.Server/Zombies/ZombieSystem.Transform.cs
@@ -93,7 +93,7 @@ public sealed partial class ZombieSystem
     private static readonly ProtoId<NpcFactionPrototype> ZombieFaction = "Zombie";
     private static readonly string MindRoleZombie = "MindRoleZombie";
     private static readonly List<ProtoId<AntagPrototype>> BannableZombiePrototypes = ["Zombie"];
-    private static readonly ProtoId<ReagentPrototype> ZombieBloodProto = "ZombieBlood";
+    private static readonly ProtoId<ReagentPrototype> ZombieBloodProto = "ZombieBlood"; // Omu - Zombie blood update
 
     /// <summary>
     /// Handles an entity turning into a zombie when they die or go into crit
