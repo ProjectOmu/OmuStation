d902377a0aa64f84e680fff0044bce6d7e61ba0e
Toast <ehallbp@gmail.com>
2026-09-13 16:15:57 -0400

    Cherrypick my goob PR

--- Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.cs
+++ Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.cs
@@ -48,6 +48,8 @@ using Robust.Shared.Physics.Components;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
+using Content.Shared.Inventory;
+using Robust.Shared.Toolshed.Commands.Values;
 using Content.Shared.Tag; // Omu
 
 namespace Content.Goobstation.Shared.MartialArts;
@@ -583,6 +585,35 @@ public abstract partial class SharedMartialArtsSystem : EntitySystem
 
         downed = IsDown(ent.Comp.CurrentTarget.Value);
         target = ent.Comp.CurrentTarget.Value;
+
+        foreach (var hand in _hands.EnumerateHands(ent.Owner)) //to do arts you have to have hands right???
+        {
+            if (!_hands.TryGetHeldItem(ent.Owner, hand, out var held))
+                continue;
+
+            if (TryComp<MartialArtBlockedComponent>(held, out var slotblockedComp)
+                    && TryComp<MartialArtsKnowledgeComponent>(ent, out var knowledgeComp)
+                    && knowledgeComp.MartialArtsForm == slotblockedComp.Form)
+            {
+                return false;
+            }
+        }
+
+        if (_inventory.TryGetSlots(ent, out var inv)) //checks for Blocked comp and type from the inventory
+        {
+            foreach (var slot in inv)
+            {
+                if (!_inventory.TryGetSlotEntity(ent, slot.Name, out var slotEnt))
+                    continue;
+
+                if (TryComp<MartialArtBlockedComponent>(slotEnt, out var slotblockedComp)
+                    && TryComp<MartialArtsKnowledgeComponent>(ent, out var knowledgeComp)
+                    && knowledgeComp.MartialArtsForm == slotblockedComp.Form)
+                {
+                    return false;
+                }
+            }
+        }
 
         if (!knowledgeComponent.Blocked)
             return true;
