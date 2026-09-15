9cf24ed82347bf69d5ba6fdd2b3f3e7190987828
Liam <lhw2837@rit.edu>
2026-09-15 14:08:06 -0400

    Let's see how this works

--- Content.IntegrationTests/Tests/Power/StationPowerTests.cs
+++ Content.IntegrationTests/Tests/Power/StationPowerTests.cs
@@ -42,7 +42,7 @@ public sealed class StationPowerTests
         "Cog",
         "Snowball",
         "Exo",
-        "Tram2"
+        "Tram2" // Omu - Port tramstation from wizden
     ];
 
     [Explicit]
