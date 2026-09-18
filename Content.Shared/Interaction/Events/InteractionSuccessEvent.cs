c457f731394df6382409ecf6f1f4780b403eedc0
Marikiii <tomastome.costa@gmail.com>
2026-09-18 15:55:37 +0100

    Update InteractionSuccessEvent.cs

--- Content.Shared/Interaction/Events/InteractionSuccessEvent.cs
+++ Content.Shared/Interaction/Events/InteractionSuccessEvent.cs
@@ -4,10 +4,8 @@ namespace Content.Shared.Interaction.Events;
 
 /// <summary>
 /// Raised on the target when successfully petting/hugging something.
-/// Actor as a nullable variable to add more ways to use this event (OMU)
 /// </summary>
 // TODO INTERACTION
 // Rename this, or move it to another namespace to make it clearer that this is specific to "petting/hugging" (InteractionPopupSystem)
 [ByRefEvent]
 public readonly record struct InteractionSuccessEvent(EntityUid User);
-
